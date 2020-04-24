a21eaded452f7dff1b630762c36c6ab139cea524
Gregg Franklin <greggfranklin@gmail.com>
2020-04-24 11:36:13 -0700

    Add Dashboard link to menu / screenshot code for child theme

--- blueprint.php
+++ blueprint.php
@@ -3,12 +3,16 @@
  * Dynamic Blueprint
  * Version: 1.1.0
  */
+ 
+ $admin_user="testadmin";
+ $password="password";
+ $admin_email="testadmin@$siteName";
 
 /** Download the latest version of WordPress*/
 ds_cli_exec( "wp core download" );
 
 //** Install WordPress
-ds_cli_exec( "wp core install --url=$siteName --title='Dynamic Blueprint' --admin_user=testadmin --admin_password=password --admin_email=pleaseupdate@$siteName" );
+ds_cli_exec( "wp core install --url=$siteName --title='Dynamic Blueprint' --admin_user=$admin_user --admin_password=$password --admin_email=$admin_email" );
 
 //** Change the tagline
 ds_cli_exec( "wp option update blogdescription 'The sites tagline'" );
@@ -28,7 +32,7 @@ ds_cli_exec( "wp rewrite structure '/%postname%/'" );
 //** Discourage search engines from indexing this site
 ds_cli_exec( "wp option update blog_public 'on'" );
 
-/** Download plugin from repository, unzip WPSiteSync for Content, activate */
+//** Download plugin from repository, unzip WPSiteSync for Content, activate
 ds_cli_exec( "wp plugin install wpsitesynccontent" );
 ds_cli_exec( "wp plugin activate wpsitesynccontent" );
 
@@ -49,24 +53,26 @@ ds_cli_exec( "wp plugin activate all-in-one-seo-pack" );
 
 /** Install & Activate Plugin located on the Computer - Use Path based on DS-CLI */
 /* Mac example */
-//ds_cli_exec( "cp /Volumes/Data/Dropbox/WP_Pithy/2.\ BBeaver\ Stuff/1.\ Beaver\ Builder\ Master\ zips/bb-plugin-agency.zip ./; wp plugin install bb-plugin-agency.zip --activate; rm bb-plugin-agency.zip" );
+//ds_cli_exec( "cp /Volumes/Data/Premium_Plugins/bb-plugin-agency.zip ./; wp plugin install bb-plugin-agency.zip --activate; rm bb-plugin-agency.zip" );
 /* Windows example */
-//ds_cli_exec( "cp c:/My_Premium_Plugins/bb-plugin-agency.zip ./; wp plugin install bb-plugin-agency.zip --activate; rm bb-plugin-agency.zip" );
+//ds_cli_exec( "cp c:/Premium_Plugins/bb-plugin-agency.zip ./; wp plugin install bb-plugin-agency.zip --activate; rm bb-plugin-agency.zip" );
 
-//** Install Theme and Activate - Use Path based on DS-CLI
+//** Install & Activate Theme located on the Computer - Use Path based on DS-CLI
 /* Mac example */
-//ds_cli_exec( "cp /Volumes/Data/Dropbox/WP_Pithy/2.\ BBeaver\ Stuff/1.\ Beaver\ Builder\ Master\ zips/bb-theme.zip ./; wp theme install bb-theme.zip; rm bb-theme.zip" );
+//ds_cli_exec( "cp /Volumes/Data/Premium_Themes/bb-theme.zip ./; wp theme install bb-theme.zip; rm bb-theme.zip" );
 /* Windows example */
-//ds_cli_exec( "cp c:/My_Premium_Themes/bb-theme.zip ./; wp theme install bb-theme.zip; rm bb-theme.zip" );
+//ds_cli_exec( "cp c:/Premium_Themes/bb-theme.zip ./; wp theme install bb-theme.zip; rm bb-theme.zip" );
 
 //** Create child theme
-ds_cli_exec( "wp scaffold child-theme astra-child --parent_theme=astra --activate" );
+$child_theme_name = "astra-child";
+ds_cli_exec( "wp scaffold child-theme $child_theme_name --parent_theme=astra --activate" );
+ds_cli_exec( "cp screenshot.png wp-content/themes/$child_theme_name/");
 
 //** Remove Default Themes (Except twentytwenty for debugging)
 ds_cli_exec( "wp theme delete twentynineteen" );
 ds_cli_exec( "wp theme delete twentyseventeen" );
 
-//** Remove Plugins
+//** Remove Default Plugins
 ds_cli_exec( "wp plugin delete akismet" );
 ds_cli_exec( "wp plugin delete hello" );
 
@@ -75,12 +81,12 @@ ds_cli_exec( "wp post delete 1 --force" ); // Hello World!
 ds_cli_exec( "wp post delete 2 --force" ); // Sample Page
 
 //** Make a new page for the homepage and blog page
-ds_cli_exec( "wp post create --post_type=page --post_title='Home' --post_status='publish'" ); // Home page
-ds_cli_exec( "wp post create --post_type=page --post_title='News' --post_status='publish'" ); // Blog page 
-ds_cli_exec( "wp post create --post_type=page --post_title='Contact' --post_status='publish' --post_content='This is my Contact Page'" ); //Contact page
+ds_cli_exec( "wp post create --post_type=page --post_title='Home' --post_status='publish' --post_author=1 --menu_order=1" ); // Home page
+ds_cli_exec( "wp post create --post_type=page --post_title='News' --post_status='publish' --post_author=1 --menu_order=2" ); // Blog page 
+ds_cli_exec( "wp post create --post_type=page --post_title='Contact' --post_status='publish' --post_content='This is my Contact Page' --post_author=1 --menu_order=4" ); //Contact page
 
 # Create post with content from given file
-ds_cli_exec( "wp post create ./about-page-content.txt --post_type=page --post_title='About' --post_status='publish'" ); //About page 
+ds_cli_exec( "wp post create ./about-page-content.txt --post_type=page --post_title='About' --post_status='publish' --post_author=1 --menu_order=3" ); //About page 
 
 //** Make the created pages the default for Home and Blog
 ds_cli_exec( "wp option update show_on_front 'page'" );
@@ -93,16 +99,17 @@ ds_cli_exec( "wp comment delete 1" );
 //** Delete all the default sidebar widgets
 ds_cli_exec( "wp widget delete search-2 recent-posts-2 recent-comments-2 archives-2 categories-2 meta-2 --quiet" );
 
-//** set the timezone
+//** Set the timezone
 ds_cli_exec( "wp option update timezone_string 'America/Los_Angeles'" );
 
-//** create the main menu and assign it to the primary menu slot
+//** Create the main menu and assign it to the primary menu slot
 ds_cli_exec( "wp menu create 'Main Menu'" );
 ds_cli_exec( "wp menu location assign main-menu primary" );
 ds_cli_exec( "wp menu item add-post main-menu 4 --title='Home'" );
 ds_cli_exec( "wp menu item add-post main-menu 5 --title='News'" );
 ds_cli_exec( "wp menu item add-post main-menu 6 --title='Contact'" );
 ds_cli_exec( "wp menu item add-post main-menu 7 --title='About'" );
+ds_cli_exec( "wp menu item add-custom main-menu Dashboard $siteName/wp-admin" );
 
 //** Create a local Git Repo
 ds_cli_exec( "git init");
@@ -149,12 +156,22 @@ ds_cli_exec( "wp user create bob bob@example.com --role=editor" );
 ds_cli_exec( "wp plugin install wordpress-importer --activate");
 ds_cli_exec( "wp import ./wordpress-wxr-example.xml --authors=create");
 
+//** Deactivate and remove WordPress Imported plugin is required
+ds_cli_exec( "wp plugin uninstall wordpress-importer --deactivate");
+
+//** Create dummy content for posts
+//ds_cli_exec( "curl http://loripsum.net/api/4 | wp post generate --post_content --count=10");
+
+//** Import image from remote site and assign to post 1 as a featured image
+//ds_cli_exec( "wp media import https://s.w.org/style/images/wp-header-logo.png --post_id=1 --featured_image");
+
 //** Check if index.php unpacked okay
 if ( is_file( "index.php" ) ) {
 
-	//** Cleanup the empty folder, download, and this script.
+	//** Cleanup the files in this script.
 	ds_cli_exec( "rm blueprint.php" );	
 	ds_cli_exec( "rm index.htm" );
 	ds_cli_exec( "rm about-page-content.txt" );
 	ds_cli_exec( "rm wordpress-wxr-example.xml" );
+	ds_cli_exec( "rm screenshot.png" );
 }
